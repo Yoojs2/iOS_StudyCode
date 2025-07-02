c590a00d8ac36b1915e0a5cbb45a1b3920bd899b
JaesungYoo <yjsyjs9633@gmail.com>
2025-07-02 15:22:29 +0900

    image view 예제 업로드

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        
+        lblHello.text = "hello, " + txtName.text!
+        
+    }
+    
 
 }
 
