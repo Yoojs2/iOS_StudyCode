ecfc4bfcf3969d12ddc51fb6943bb72d174d2b12
JaesungYoo <yjsyjs9633@gmail.com>
2025-07-04 23:33:25 +0900

    PickerView에 이미지가 보이게 수정

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
     var imageFileName = ["1.PNG", "2.PNG", "3.PNG", "4.PNG", "5.PNG", "6.PNG", "7.PNG"]
     var imageArray = [UIImage?]()
     
+    //피커뷰의 높이
+    let PICKER_VIEW_HEIGHT: CGFloat = 80
+    
     @IBOutlet var pickerImage: UIImageView!
     @IBOutlet var lbImageFileName: UILabel!
     @IBOutlet var imageView: UIImageView!
@@ -42,18 +45,30 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
         return imageFileName.count
     }
     
-    //피커 뷰에게 컴포넌트의 각 열의 타이틀을 문자자열의 값으로 넘겨줌
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imageFileName[row]
+    //피커 뷰에게 컴포넌트의 각 열의 타이틀을 문자자열의 값으로 넘겨줌 (titleForRow는 타이틀을 정의함)
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileName[row]
+//    }
+    
+    //피커 뷰에게 컴포넌트 각 열의 view를 넘겨줌
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image:imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        
+        return imageView
     }
+    
     
     //룰렛을 선택했을때 실행 됨
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         lbImageFileName.text = imageFileName[row]
         imageView.image = imageArray[row]
-        
     }
     
+    //피커뷰의 높이를 전달한다
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
     
     
     
