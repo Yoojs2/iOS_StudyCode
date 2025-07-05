4faef977225da47d7440a692130732f01a029cdd
JaesungYoo <yjsyjs9633@gmail.com>
2025-07-05 16:42:42 +0900

    Alert 완성

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -9,11 +9,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    let imgRemove = UIImage(named: "lamp-remove.png")
+    
+    var isLampOn = true
+    
+    @IBOutlet var lampImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lampImg.image = imgOn
     }
+    
+    @IBAction func btnLampOn(_ sender: UIButton) {
+        
+        if(isLampOn){
+            
+           let lampOnAlert = UIAlertController(title: "경고", message: "Lamp is On", preferredStyle: .alert)
+            let onAction = UIAlertAction(title: "네, 알겠습니다.", style: UIAlertAction.Style.default, handler: nil)
+            lampOnAlert.addAction(onAction)
+            present(lampOnAlert, animated: true, completion : nil)
+            
+        } else{
+            
+            lampImg.image = imgOff
+            isLampOn = true
+        }
+    }
+    
 
-
+    @IBAction func btnLampOff(_ sender: UIButton) {
+        
+        if (isLampOn){
+            //1번
+            let lampOffAlert = UIAlertController(title: "램프 끄기", message: "램프를 끄겠습니까", preferredStyle: .alert)
+            
+            //2번
+            let offAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default,
+                                          handler: { ACTION in self.lampImg.image = self.imgOff
+            })
+            
+            //3번
+            let cencelAction = UIAlertAction(title: "취소", style: UIAlertAction.Style.cancel, handler: nil)
+            
+            //4번
+            lampOffAlert.addAction(offAction)
+            lampOffAlert.addAction(cencelAction)
+            
+            //5번
+            present(lampOffAlert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func btnLampRemove(_ sender: UIButton) {
+        
+        let lampRemoveAlert = UIAlertController(title: "램프 제거", message: "램프를 제거하시겠습니까?", preferredStyle: .alert)
+        
+        let offAction = UIAlertAction(title: "아니오, 끕니다(off)", style: UIAlertAction.Style.default, handler: {ACTION in
+            self.lampImg.image=self.imgOff
+            self.isLampOn=false})
+        
+        let onAction = UIAlertAction(title: "아니오 켭니다", style: UIAlertAction.Style.default, handler: {ACTION in
+            self.lampImg.image=self.imgOn
+            self.isLampOn=true})
+        
+        
+        let removeAction = UIAlertAction(title: "네, 제거합니다", style: UIAlertAction.Style.default, handler: {ACTION in self.lampImg.image=self.imgRemove
+            self.isLampOn=false
+        })
+        
+        lampRemoveAlert.addAction(offAction)
+        lampRemoveAlert.addAction(onAction)
+        lampRemoveAlert.addAction(removeAction)
+        
+        present(lampRemoveAlert, animated: true, completion: nil)
+    }
+    
+    
 }
 
--- SwiftGrammer/SwiftGrammer/main.swift
+++ SwiftGrammer/SwiftGrammer/main.swift
@@ -7,11 +7,49 @@
 
 import Foundation
 
-
 print("Hello, World!")
-
 
 var Name = "Ho-Jeong"
 var score = 100
 
 var name: [String] = ["슈퍼맨", "배트맨", "캡틴", "아이언맨"]
+
+func testfunc(parm: Int) -> (){
+    print("function call")
+}
+
+testfunc(parm: 10)
+
+//익명 함수
+
+//{ (파라미터 목록) -> 반환형 in
+//    실행 코드
+//}
+
+func completeWork(finised: Bool) ->(){
+    print("complete : \(finised)")
+}
+
+let myClosure = { (finised: Bool) -> () in
+    print("complete : \(finised)")
+}
+
+myClosure(true)  // 호출 방법
+
+
+//함수의 인자로 쓰는방법
+
+
+func doWork(completion: (Bool) -> Void) {
+    print("작업 중...")
+    completion(true)
+}
+
+//do work를 호출했고 인자로 익명함수를 쓴것
+doWork(completion: { (finised) -> () in
+    print("complete : \(finised)")
+})
+
+
+
+
